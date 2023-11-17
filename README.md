083d688c8da2c3be312623eeb58f74f5f4557818
John Francis <jfrancis@turing.ac.uk>
2023-11-17 11:16:18 +0000

    basic setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # canopy-quality-scivision
 Trained model for identifying pixels containing trees as well as their heights.
+
+This repository contains the refactored code for [scivision](https://github.com/alan-turing-institute/scivision). The original source code is available at https://github.com/johnfrancis13/where_to_plant_trees. 
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+requirements = []
+with open("requirements.txt") as f:
+    for line in f:
+        stripped = line.split("#")[0].strip()
+        if len(stripped) > 0:
+            requirements.append(stripped)
+
+setup(
+    name="canopy_quality",
+    version="0.0.1",
+    description="scivision plugin, using Canopy Quality model",
+    url="https://github.com/johnfrancis13/canopy-quality-scivision",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.7",
+)
