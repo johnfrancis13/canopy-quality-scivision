1561dc7e979ece6c55575da70e24b3141eb0b80b
John Francis <jfrancis@turing.ac.uk>
2023-11-24 15:54:44 +0000

    update test

--- test/test.py
+++ test/test.py
@@ -10,7 +10,15 @@ model = treenet(model_type = "multi_spectral")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 ## create 14 band image
-image = np.random.randint(255, size=(1, 14, 240, 240), dtype=np.uint8)
+image = np.random.randint(255, size=(14, 240, 240), dtype=np.uint8)
 
-##predict
+##predict on random
 y = model.predict(np_image=image)
+
+# predict on batch
+images = np.random.randint(255, size=(25, 14, 240, 240), dtype=np.uint8)
+y = model.predict_batch(images, batch_size=16)
+
+# predict on actual image
+a= np.load("../example_data/P_1_X0_1_X1_240_Y0_1_Y1_240_113.npy",allow_pickle=True)
+y = model.predict(np_image=a)
